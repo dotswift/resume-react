3e8724ce6bf9ed9c6679129bcb55801c281b1bc3
derek quinn <dereq@Troy.local>
2021-01-11 04:10:29 -0500

    Added resume content

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
       <div className="App">
         <Header resumeData={resumeData}/>
         <About />
-        {/* <Resume />
-        <Portfolio />
+        <Resume />
+        {/* <Portfolio />
         <Testimonials /> */}
         {/* <ContactUs /> */}
         <Footer />
