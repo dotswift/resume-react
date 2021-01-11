b34bc3836888b9a2ef1838ad61a5b118573b97ed
derek quinn <dereq@Troy.local>
2021-01-11 04:48:10 -0500

    CSS changes to nav, header updates and portfolio content

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header resumeData={resumeData}/>
+        <Portfolio />
         <About />
         <Resume />
-        {/* <Portfolio />
-        <Testimonials /> */}
+    
+       {/* <Testimonials /> */}
         {/* <ContactUs /> */}
         <Footer />
       </div>
