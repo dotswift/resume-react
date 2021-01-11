7ec29b4015d9b7324ad0b5f97144b56c8cd57732
derek quinn <dereq@Troy.local>
2021-01-10 23:55:01 -0500

    added links to footer, removed skype

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,12 +11,12 @@ export default class Footer extends Component {
         <div className="twelve columns">
           <ul className="social-links">
             {/* <li><a href="#"><i className="fa fa-facebook" /></a></li> */}
-            <li><a href="#"><i className="fa fa-twitter" /></a></li>
+            <li><a href="https://www.twitter.com/derekquinn"><i className="fa fa-twitter" /></a></li>
             {/* <li><a href="#"><i className="fa fa-google-plus" /></a></li> */}
-            <li><a href="#"><i className="fa fa-linkedin" /></a></li>
+            <li><a href="https://www.linkedin.com/in/derekq/"><i className="fa fa-linkedin" /></a></li>
             {/* <li><a href="#"><i className="fa fa-instagram" /></a></li>
             <li><a href="#"><i className="fa fa-dribbble" /></a></li> */}
-            <li><a href="#"><i className="fa fa-skype" /></a></li>
+            {/* <li><a href="derektquinn"><i className="fa fa-skype" /></a></li> */}
           </ul>
           <ul className="copyright">
             <li>© Copyright 2020</li>
